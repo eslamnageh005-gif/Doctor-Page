62103c8f9b5769af56f27a7315b9aa39ccbe9523
Eslam Nageh <eslamnageh005@gmail.com>
2026-03-03 00:00:47 +0200

    emergency fix: simplify App.tsx to remove white screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AppLayout from "@/components/AppLayout";
 import Dashboard from "./pages/Dashboard";
 import Appointments from "./pages/Appointments";
 import MedicalRecords from "./pages/MedicalRecords";
@@ -18,15 +17,11 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          {/* هنا بنقول لـ React: استخدم الـ Layout لكل الصفحات اللي جاية */}
-          <Route element={<AppLayout children={<Dashboard />} />}>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/appointments" element={<Appointments />} />
-            <Route path="/medical-records" element={<MedicalRecords />} />
-            <Route path="/medical-records/new" element={<NewMedicalRecord />} />
-            <Route path="/profile" element={<DoctorProfile />} />
-          </Route>
-          {/* صفحة الـ 404 بتكون بره الـ Layout أو جواه براحتك */}
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/appointments" element={<Appointments />} />
+          <Route path="/medical-records" element={<MedicalRecords />} />
+          <Route path="/medical-records/new" element={<NewMedicalRecord />} />
+          <Route path="/profile" element={<DoctorProfile />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
